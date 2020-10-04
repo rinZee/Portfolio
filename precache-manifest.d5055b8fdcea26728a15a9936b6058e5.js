846735a142433db5210d934699516c50aa90253d
Tsheten Renzee Sherpa <trex33160@gmail.com>
2020-10-04 23:58:52 +0545

    Updates

---
+++ precache-manifest.d5055b8fdcea26728a15a9936b6058e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7acccde7a58b598f6a23a1eb2571af",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f5d217684aedc78752eb",
+    "url": "/Portfolio/static/css/main.77e49c54.chunk.css"
+  },
+  {
+    "revision": "e4abcb7b61725429aed4",
+    "url": "/Portfolio/static/js/2.b53ed539.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.b53ed539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d217684aedc78752eb",
+    "url": "/Portfolio/static/js/main.176ebb2c.chunk.js"
+  },
+  {
+    "revision": "627bdb01d220a474fd8b",
+    "url": "/Portfolio/static/js/runtime-main.9e287cfb.js"
+  }
+]);
