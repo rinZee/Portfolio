be3b3da8c02b852e1222881c474d9f8221511f3c
Tsheten Renzee Sherpa <trex33160@gmail.com>
2021-05-04 22:07:03 +0545

    Updates

---
+++ precache-manifest.b166dcf239042dc735010376a8e2822f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084c131ea068bacd7ecb6ea6173d00a4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f6cf62ac4c8877be69e0",
+    "url": "/Portfolio/static/css/main.6f36e0a0.chunk.css"
+  },
+  {
+    "revision": "3e4ea97ed22c36ffe5da",
+    "url": "/Portfolio/static/js/2.b276562b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.b276562b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6cf62ac4c8877be69e0",
+    "url": "/Portfolio/static/js/main.5af62b91.chunk.js"
+  },
+  {
+    "revision": "627bdb01d220a474fd8b",
+    "url": "/Portfolio/static/js/runtime-main.9e287cfb.js"
+  },
+  {
+    "revision": "e3eeef7627dc59fb9796324b20f08d79",
+    "url": "/Portfolio/static/media/FoodBook.e3eeef76.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.98182e241be098d481cc44387ecfe61d.js"
+  "/Portfolio/precache-manifest.b166dcf239042dc735010376a8e2822f.js"
 );
 
 self.addEventListener('message', (event) => {
