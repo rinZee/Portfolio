61e2959d9d2d68c9421a116d2b5978ca40dc9b95
Tsheten Renzee Sherpa <trex33160@gmail.com>
2021-05-27 14:21:19 +0545

    Updates

---
+++ precache-manifest.3bad045168f2ac9298fc359e4da9b7b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9d4a1b0e161cf1030836d858d9789e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3eb773fce28e260782a7",
+    "url": "/Portfolio/static/css/main.962915eb.chunk.css"
+  },
+  {
+    "revision": "2819713e598eec179d68",
+    "url": "/Portfolio/static/js/2.89862694.chunk.js"
+  },
+  {
+    "revision": "e79898a42ec2d02d5bbd03509f17e33d",
+    "url": "/Portfolio/static/js/2.89862694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb773fce28e260782a7",
+    "url": "/Portfolio/static/js/main.bc3d0b55.chunk.js"
+  },
+  {
+    "revision": "627bdb01d220a474fd8b",
+    "url": "/Portfolio/static/js/runtime-main.9e287cfb.js"
+  },
+  {
+    "revision": "9629855c78fb946e16a4e9902290ffb0",
+    "url": "/Portfolio/static/media/Foodbook.9629855c.png"
+  },
+  {
+    "revision": "db58111515c902c7706d87036f9fd2ee",
+    "url": "/Portfolio/static/media/Microsoft Homepage clone.db581115.png"
+  },
+  {
+    "revision": "ca37d55eb1f0c10ac36f1ddf0af4efe7",
+    "url": "/Portfolio/static/media/firebase-chat.ca37d55e.png"
+  },
+  {
+    "revision": "c068395164fbb6443b502828462c93f9",
+    "url": "/Portfolio/static/media/resume.c0683951.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.6e2816eb91b98b604f3109aad4c9e68c.js"
+  "/Portfolio/precache-manifest.3bad045168f2ac9298fc359e4da9b7b9.js"
 );
 
 self.addEventListener('message', (event) => {
